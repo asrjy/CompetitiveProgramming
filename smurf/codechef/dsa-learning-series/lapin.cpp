3b2fb8d7240b0c5b4657570c73342153c60be9a8
s3p1ol <yashwanth.allakky@codingstudio.club>
2020-05-08 21:08:38 +0530

    lapin

--- smurf/codechef/dsa-learning-series/lapin.cpp
+++ smurf/codechef/dsa-learning-series/lapin.cpp
@@ -1,11 +1,28 @@
 #include<bits/stdc++.h>
-using mainspace std;
+using namespace std;
 int main(){
     int i(0);
-    string s;
+    string s, s1, s2;
     cin >> i;
     while(i--){
 	cin >> s;
-
+	if(s.length() % 2 == 0){
+	    s1 = sort(s.begin(), s + s.length()/2 - 1);
+	    s2 = sort(s + s.length()/2, s.end());
+	    if(s1 == s2){
+		cout << "YES" << endl;
+	    } else {
+		cout << "NO" << endl;
+	    }
+	} else {
+	    s1 = sort(s.begin(), s + s.length()/2 -1);
+	    s2 = sort(s+ s.length()/2 + 1, s.end());
+	    if(s1 == s2){
+		cout << "YES" << endl;
+	    } else {
+		cout << "NO" << endl;
+	    }
+	}
     }
+    return 0;
 }
