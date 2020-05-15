b055de2ce47780f21da115acfd85d5093c42411b
s3p1ol <yashwanth.allakky@codingstudio.club>
2020-05-15 21:45:06 +0530

    275A

--- smurf/codeforces/275A.cpp
+++ smurf/codeforces/275A.cpp
@@ -12,14 +12,14 @@ int main(){
 	    tot++;
 	}
     }
-    ((inps[0]+inps[1]+inps[3]+inps[4])%2==0) ? cout << 1 : cout << 0;
-    ((tot-inps[6]-inps[7]-inps[8])%2==0) ? cout << 1 : cout << 0;
-    ((inps[1]+inps[2]+inps[4]+inps[5])%2==0) ? cout << 1 << endl : cout << 0 << endl;
-    ((tot-inps[2]-inps[5]-inps[8])%2==0) ? cout << 1 : cout << 0;
-    ((tot)%2==0) ? cout << 1 : cout << 0;
-    ((tot-inps[3]-inps[0]-inps[6])%2==0) ? cout << 1 << endl : cout << 0 << endl;
-    ((inps[3]+inps[4]+inps[6]+inps[7])%2==0) ? cout << 1 : cout << 0;
-    ((tot - inps[0]-inps[1]-inps[2])%2==0) ? cout << 1 : cout << 0;
-    ((inps[4]+inps[5]+inps[7]+inps[8])%2==0) ? cout << 1 << endl : cout << 0 << endl;
+    ((inps[0]+inps[1]+inps[3])%2==0) ? cout << 1 : cout << 0;
+    ((tot-inps[6]-inps[7]-inps[8]-inps[3]-inps[5])%2==0) ? cout << 1 : cout << 0;
+    ((inps[1]+inps[2]+inps[5])%2==0) ? cout << 1 << endl : cout << 0 << endl;
+    ((tot-inps[2]-inps[5]-inps[8]-inps[1]-inps[7])%2==0) ? cout << 1 : cout << 0;
+    ((tot-inps[0]-inps[2]-inps[6]-inps[8])%2==0) ? cout << 1 : cout << 0;
+    ((tot-inps[3]-inps[0]-inps[6]-inps[1]-inps[7])%2==0) ? cout << 1 << endl : cout << 0 << endl;
+    ((inps[3]+inps[6]+inps[7])%2==0) ? cout << 1 : cout << 0;
+    ((tot - inps[0]-inps[1]-inps[2]-inps[3]-inps[5])%2==0) ? cout << 1 : cout << 0;
+    ((inps[5]+inps[7]+inps[8])%2==0) ? cout << 1 << endl : cout << 0 << endl;
     return 0;   
 }
