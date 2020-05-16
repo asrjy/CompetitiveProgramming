da11b7727d0a156f06742002d01db6600c537735
s3p1ol <yashwanth.allakky@codingstudio.club>
2020-05-16 18:43:47 +0530

    233A

--- smurf/codeforces/61A.cpp
+++ smurf/codeforces/61A.cpp
@@ -5,11 +5,10 @@ int main(){
     int i;
     string a, b;
     cin >> a >> b;
-    for(i =0 ; i < a.length(); i++){
+    for(i =0 ; i < a.length(); i++)
 	if(a[i] == b[i])
 	    cout << "0";
 	else 
 	    cout << "1";
-    }
     return 0;
 }
