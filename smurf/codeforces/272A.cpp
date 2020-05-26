1b38ad0b8460306578a126d764980d49a0ea3bf6
s3p1ol <yashwanth.alakky@gmail.com>
2020-05-26 20:59:07 +0530

    272A

--- smurf/codeforces/272A.cpp
+++ smurf/codeforces/272A.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int n, x, sum(-1), ans(0); cin >> n;
+	int n, x, sum(0), ans(0); cin >> n;
 	for(int i =0; i < n; i++){
 		cin >> x;
 		sum += x;
 	}
-	for(int i = 0; i < 5; i++)
-		if((sum+i)%(n+1) != 0){
-			cout << "can show " << i << endl;
+	//cout << "sum " << sum << endl;
+	for(int i = 1; i <= 5; i++)
+		if((sum+i-1)%(n+1) != 0){
+			//cout << "can show " << i << endl;
 			ans++;
 		}
 	cout << ans << endl ;
