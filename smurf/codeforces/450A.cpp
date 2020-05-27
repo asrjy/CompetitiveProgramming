e222607a19f7d3c4bbc04ec855d9efe38392b565
s3p1ol <yashwanth.alakky@gmail.com>
2020-05-27 20:58:15 +0530

    165A

--- smurf/codeforces/450A.cpp
+++ smurf/codeforces/450A.cpp
@@ -7,12 +7,27 @@ int main(){
 	for(i =0; i < n; i++)
 		cin >> arr[n];
 	for(i =0; i < n, x < n; i++){
-		if(arr[i] != -1 && arr[i] <= a){
-			arr[i] = -1;
-			ans = i;
-			x++;
-		} else if(arr[i] != -1 && arr[i] > a){
-			arr[i] -= a;
+		if(i == n-1){
+			if(arr[i] != -1 && arr[i] <= a){
+				arr[i] = -1;
+				ans = i;
+				x++;
+				if(x == n)
+					break;
+				else
+					i = 0;
+			} else if(arr[i] != -1 && arr[i] > a){
+				arr[i] -= a;
+				i = 0;
+			}
+		} else {
+			if(arr[i] != -1 && arr[i] <= a){
+				arr[i] = -1;
+				ans = i;
+				x++;
+			} else if(arr[i] != -1 && arr[i] > a){
+				arr[i] -= a;
+			}
 		}
 	}
 	cout << ans << endl;
