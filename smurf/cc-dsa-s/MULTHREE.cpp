b0cf374c77eeb1736524d847d5b693d224432279
Yashwanth <yashwanth.alakky@gmail.com>
2021-11-19 19:23:31 +0530

    MULTHREE

--- smurf/cc-dsa-s/MULTHREE.cpp
+++ smurf/cc-dsa-s/MULTHREE.cpp
@@ -6,16 +6,25 @@ int main(){
     while(t--){
         long long int k, d0, d1;
         cin >> k >> d0 >> d1;
-        long long sum = d0 + d1, last = (d0 + d1)%10;
-        for(int i =0; i < k-2; i++){
-            sum += last;
-            last = sum % 10;
+        int d2 = (d0 + d1)%10;
+        int p = (k-3)/4;
+        int rem = k-3-p;
+        long long int s = (2*(d0 + d1))%10 + (4*(d0 + d1))%10 + (8*(d0 + d1))%10 + (6*(d0 + d1))%10;
+        long long int sum = d0 + d1 + d2 + (p*s);
+        // cout << sum << endl;
+        cout << rem << endl;
+        if(rem == 1){
+            sum += (2*(d0 + d1))%10;
+        } else if(rem == 2){
+            sum += (2*(d0 + d1))%10 + (4*(d0 + d1))%10;
+        } else if(rem == 3){
+            sum += (2*(d0 + d1))%10 + (4*(d0 + d1))%10 + (8*(d0 + d1))%10;
         }
-        if(sum % 3 == 0){
+        // cout << sum << endl;
+        if(sum%3 == 0)
             cout << "YES" << endl;
-        } else {
+        else 
             cout << "NO" << endl;
-        }
     }
     return 0;
 }
